65ce27240b93376cbaef25028bdf51a1223ff06b
zak5991 <zakaria.chraa@gmail.com>
2024-05-20 13:25:03 -0700

    update

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include "main.h"
-
-/**
- * main - entry point
- *
- * Return: generated password
- */
 
 int main(void)
 {
-	char c;
-	int x;
+  int n;
+  int a[5];
+  int *p;
 
-	srand(time(0));
-	while (x <= 2645)
-	{
-		c = rand() % 128;
-		x += c;
-		putchar(c);
-	}
-	putchar(2772 - x);
-
-	return (0);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
