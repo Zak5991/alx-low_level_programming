ed0cf1638a18706c083d94f118cef24c11639470
zak5991 <zakaria.chraa@gmail.com>
2024-05-20 13:32:26 -0700

    update

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+/**
+ * main - creates randomized valid passwords for
+ * 101-crackme printed to standard out
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	int sum = 0;
+	char randchar;
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	srand(time(0));
+
+	while (sum <= 2646)
+	{
+		randchar = rand() % 128;
+		write(1, &randchar, 1);
+		sum += randchar;
+	}
+
+	randchar = 2772 - sum;
+	write(1, &randchar, 1);
+	return (0);
 }
